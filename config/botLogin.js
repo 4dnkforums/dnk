e23cbf1f80e9e4ef39e7ad9e7e5ccb3e95d0eac5
dnkgt <justraylengamer@gmail.com>
2019-05-23 21:10:17 +0300

    dnkro

---
+++ config/botLogin.js
@@ -0,0 +1,18 @@
+// The bot only requires a token if you have a bot account
+// You can get a bot token at https://discordapp.com/developers/applications/me
+
+// Youtube API is needed to use the search features
+// otherwise you will keep getting bad request
+
+// Owner id is required to use the admin commands
+// you can get this id if you have developer options enabled
+// in discord. Then just right click your username and copy id
+
+// **WARNING** If you do not add a Owner ID or have a group that is in the "preference.json" file
+// Then no one will be able to use the admin commands
+
+module.exports = {
+	token: "NTcxNzE1MjEzMDU3ODUxMzky.XN-rdQ.U0Qycvu2UWgtPlONWb41MzJZr6c",
+	youtubeAPI: "AIzaSyCf7AFwXL45Tk5Fuas5PgfSrrC8V37zW5U",
+	owner_id: "372052771798515723"
+}
